38ced3c6578c5b416612a1e463c330abd12a9cf0
Muhammad Usman Qadeer <usmanqadeer463@gmail.com>
2022-09-28 12:51:00 -0700

    Add Jquery on form and ui set

--- myscript.js
+++ myscript.js
@@ -1,29 +1,58 @@
 
-  
-    $(document).ready(function(){
-        $('#MybtnModal').click(function(){
-            $('#Mymodal').modal('show')
-        });
-        $('.MybtnModal2').click(function(){
-            $('#Mymodal').modal('show')
-        });
-    });
 
-    function deleterecord(){
-        swal({
-            title: "Are you sure?",
-            text: "Once deleted, you will not be able to recover this imaginary data!",
-            icon: "warning",
-            buttons: true,
-            dangerMode: true,
-          })
-          .then((willDelete) => {
-            if (willDelete) {
-              swal("Poof! Your imaginary file has been deleted!", {
-                icon: "success",
-              });
-            } else {
-              swal("Your imaginary file is safe!");
-            }
-          });
-    };
+$(document).ready(function () {
+  $('#MybtnModal').click(function () {
+    $('#Mymodal').modal('show')
+  });
+  $('.MybtnModal2').click(function () {
+    $('#Mymodal').modal('show')
+  });
+  $("#Myform").submit(function (event) {
+    event.preventDefault();
+    $('#fName').val();
+    $('#lName').val();
+    $('#inputEmail4"').val();
+    $('#inputPassword4').val();
+    $('#inputAddress').val();
+    $('#age').val();
+    $('#inputCity').val();
+    $('#inputState').val();
+    $('#inputZip').val();
+    $('#inputState').val();
+    $('#radio').val();
+    $("input[type='radio']").val();
+    $("input[type='checkbox']").val();
+  });
+});
+
+
+function deleterecord() {
+  swal({
+    title: "Are you sure?",
+    text: "Once deleted, you will not be able to recover this imaginary data!",
+    icon: "warning",
+    buttons: true,
+    dangerMode: true,
+  })
+    .then((willDelete) => {
+      if (willDelete) {
+        swal("Poof! Your imaginary file has been deleted!", {
+          icon: "success",
+        });
+      } else {
+        swal("Your imaginary file is safe!");
+      }
+    });
+};
+
+function addUser(event) {
+  event.preventDefault();
+
+  var nbr;
+  nbr = document.getElementById("age").value;
+  if (isNaN(nbr)) {
+    alert("Enter the Number");
+  }
+  return false;
+
+}
